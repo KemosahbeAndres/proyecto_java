d2587e078ebcefce867f0cc87fdad24cf2de7712
Andres Cubillos <andrestj1996@gmail.com>
2022-10-04 14:37:52 -0300

    Metodo main casi listo.

--- src/stomas/andres/App.java
+++ src/stomas/andres/App.java
@@ -1,10 +1,38 @@
 package stomas.andres;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        List<Orden> ordenes = new ArrayList<Orden>();
+        boolean run = true;
+        while(run){
+            int opcion = menu();
+            if(opcion == 0) run = false;
+        }
+    }
+    public static int menu(){
+        System.out.println("SISTEMA ORDENES DE COMPRA");
+        System.out.println("1. Crear Orden");
+        System.out.println("2. Ver Ordenes");
+        System.out.println("3. Eliminar Orden");
+        System.out.println("0. Salir");
+        return getInt();
+    }
+    public static int getInt(){
+        return getInt("> ");
+    }
+    public static int getInt(String mensaje){
+        try{
+            System.out.print(mensaje.trim() + " ");
+            return sc.nextInt();
+        }catch(Exception e){
+            System.out.println("Debes ingresar un numero entero!");
+            sc.nextLine();
+            return getInt(mensaje);
+        }
     }
 }
